7f723b5fd2b0f9bad36556b133c8fe0a49bd7b3c
MarianIsrael <marianwadia55@gmail.com>
2024-02-22 12:09:14 +0200

    added deleteUser && getUsers handlers

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { register, login, logoutAll } = require("../controllers/userController");
+const { register, login, logoutAll, getAllUsers, deleteUser } = require("../controllers/userController");
 const { checkAdmin, accessUser } = require("../middlewares/middleware")
 const router = express.Router();
 
 router.post("/signin", login);
 router.post("/signup", checkAdmin ,register);
 router.patch("/logout", accessUser, logoutAll);
-
+router.get("/users", checkAdmin, getAllUsers)
+router.delete("/users/:userId", checkAdmin, deleteUser);
 
 module.exports = router;
