7b8c174428e5ec65cadf17fbf8b4bc13bd65b976
MarianIsrael <marianwadia55@gmail.com>
2024-02-22 12:08:23 +0200

    removed deleteRoute

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const {getUserProfile, deleteUserProfile, updateUserProfile} = require('../controllers/profileController');
+const {getUserProfile, updateUserProfile} = require('../controllers/profileController');
 const { accessUser } = require('../middlewares/middleware');
 
 const router = express.Router();
 
 router.get('/profile', accessUser, getUserProfile);
 router.put('/profile', accessUser, updateUserProfile);
-router.delete('/profile', accessUser, deleteUserProfile);
 
 module.exports = router;
