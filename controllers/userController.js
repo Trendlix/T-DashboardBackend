019a38b1255c09afcc9a9e17f14d6460859e0788
marianWadia <marianwadia55@gmail.com>
2024-05-15 12:52:55 +0300

    added correct frontend deployed url

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,10 +22,10 @@ const login = async function (req, res, next) {
     user.save(); 
     const { password, ...info } = user._doc;
     if(info.role === 'super'){
-      res.cookie("adminToken", token, { httpOnly: false, })
-    }else{
-      res.cookie("accessToken", token, { httpOnly: false, })
-    }
+      res.cookie("adminToken", token, { httpOnly: false, secure: process.env.NODE_ENV === 'production', sameSite: 'None' })
+    }else
+      res.cookie("accessToken", token, { httpOnly: false, secure: process.env.NODE_ENV === 'production', sameSite: 'None' })
+    
       res.status(200).json(info);
   } catch (err) {
     console.log(err)
